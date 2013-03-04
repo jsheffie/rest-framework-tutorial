2b27025b64b01e2373cbc9e4be3b4f159371d17f
Jeff Sheffield <jeff.sheffield@gmail.com>
2013-03-04 13:27:55 -0600

    remove comments

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -4,12 +4,6 @@ from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
-
-#from django.http import HttpResponse
-#from django.views.decorators.csrf import csrf_exempt
-#from rest_framework.renderers import JSONRenderer
-#from rest_framework.parsers import JSONParser
-
 @api_view(['GET','POST'])
 def snippet_list(request):
     """ List all code snippets, or create a new snippet."""
