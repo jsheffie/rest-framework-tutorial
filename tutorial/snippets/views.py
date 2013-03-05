8adc0bf6c8d2436c0eb720b522e5f728f7277f92
Jeff Sheffield <jeff.sheffield@gmail.com>
2013-03-05 12:04:48 -0600

    tut4: pre object level perms ( beginning of a perms infrastructure/working)

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,18 +1,40 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
+from django.contrib.auth.models import User
+
 from django.http import Http404
 from rest_framework import mixins
 from rest_framework import generics
+
+from rest_framework import permissions
+
 
 class SnippetList(generics.ListCreateAPIView):
     """ List all code snippets, or create a new snippet."""
     model = Snippet
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    
+    def pre_save(self, obj):
+        obj.owner = self.request.user
         
+    
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """ Retrieve, update or delete a code snippet."""
     model = Snippet
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def pre_save(self, obj):
+        obj.owner = self.request.user
+    
+class UserList(generics.ListAPIView):
+    model = User
+    serializer_class = UserSerializer
+    
+class UserDetail(generics.RetrieveAPIView):
+    model = User
+    serializer_class = UserSerializer
     
 # Note: Edge cases we are not dealing with yet
 #       - malformed json, 
