d4dcfcf9b6810456a804bebbcc1e293eaf6d31ee
Jeff Sheffield <jeff.sheffield@gmail.com>
2013-03-05 10:59:22 -0600

    whopse: NOW finished tut3

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -4,17 +4,12 @@ from django.http import Http404
 from rest_framework import mixins
 from rest_framework import generics
 
-class SnippetList(mixins.ListModelMixin, 
-                  mixins.CreateModelMixin, 
-                  generics.MultipleObjectAPIView):
+class SnippetList(generics.ListCreateAPIView):
     """ List all code snippets, or create a new snippet."""
     model = Snippet
     serializer_class = SnippetSerializer
         
-class SnippetDetail(mixins.RetrieveModelMixin, 
-                    mixins.UpdateModelMixin, 
-                    mixins.DestroyModelMixin, 
-                    generics.SingleObjectAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """ Retrieve, update or delete a code snippet."""
     model = Snippet
     serializer_class = SnippetSerializer
